4e275f83e8d7e05b930b178f5c7257b9744314eb
Kevin Läufer <laeufer@cs.berkeley.edu>
2024-08-08 17:01:36 -0400

    add command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+// Copyright 2024 Cornell University
+// released under MIT License
+// author: Kevin Laeufer <laeufer@cornell.edu>
+
+use clap::{arg, Parser};
+use patronus::*;
+
+#[derive(Parser, Debug)]
+#[command(name = "patron")]
+#[command(author = "Kevin Laeufer <laeufer@cornell.edu>")]
+#[command(version)]
+#[command(about = "Tries to find a witness that shows how to get to a bad state.", long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    verbose: bool,
+    #[arg(value_name = "BTOR2", index = 1)]
+    filename: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let (ctx, sys) = btor2::parse_file(&args.filename).expect("Failed to load btor2 file!");
 }
